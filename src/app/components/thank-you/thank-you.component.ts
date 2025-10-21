a1027b509baaceda4d00b44b57e11e3ef9007fbe
Adrien Baffioni <baffioniadrien23@gmail.com>
2025-10-21 11:19:20 +0200

    [ThankYou] remove unused import

--- src/app/components/thank-you/thank-you.component.ts
+++ src/app/components/thank-you/thank-you.component.ts
@@ -1,13 +1,6 @@
-import {Component, EventEmitter, inject, OnInit, Output} from '@angular/core';
-import {AddOns} from '../../shared/models/add-ons';
-import {Plan} from '../../shared/models/plan';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {Component, inject, OnInit} from '@angular/core';
 import {ScreenService} from '../../shared/services/screen.service';
 import {TextService} from '../../shared/services/text.service';
-import {AddOnsService} from '../../shared/services/add-ons.service';
-import {StepValidationService} from '../../shared/services/step-validation.service';
-import {FormDataService} from '../../shared/services/form-data.service';
-import {forkJoin} from 'rxjs';
 
 @Component({
   selector: 'app-thank-you',
