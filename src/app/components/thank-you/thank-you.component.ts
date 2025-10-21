602c3b786971cd31202eacc359f3b75866d06e9e
Adrien Baffioni <baffioniadrien23@gmail.com>
2025-10-21 11:18:26 +0200

    [ThankYou] implement thank-you page layout and logic

--- src/app/components/thank-you/thank-you.component.ts
+++ src/app/components/thank-you/thank-you.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, inject, OnInit, Output} from '@angular/core';
+import {AddOns} from '../../shared/models/add-ons';
+import {Plan} from '../../shared/models/plan';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {ScreenService} from '../../shared/services/screen.service';
+import {TextService} from '../../shared/services/text.service';
+import {AddOnsService} from '../../shared/services/add-ons.service';
+import {StepValidationService} from '../../shared/services/step-validation.service';
+import {FormDataService} from '../../shared/services/form-data.service';
+import {forkJoin} from 'rxjs';
 
 @Component({
   selector: 'app-thank-you',
@@ -6,6 +15,17 @@ import { Component } from '@angular/core';
   templateUrl: './thank-you.component.html',
   styleUrl: './thank-you.component.scss'
 })
-export class ThankYouComponent {
+export class ThankYouComponent implements OnInit {
+  text: { [p: string]: any } = {};
+  screen = inject(ScreenService);
 
+  constructor(
+    private textService: TextService) {
+  }
+
+  ngOnInit() {
+    this.textService.getThankYouText().subscribe((data) => {
+      this.text = data;
+    })
+  }
 }
